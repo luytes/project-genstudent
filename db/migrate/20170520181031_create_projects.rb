8049d8a931d1c9e6487edacfe672be5712487f6a
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-05-20 20:12:49 +0200

    changed the things and added lat and long and address

--- db/migrate/20170520181031_create_projects.rb
+++ db/migrate/20170520181031_create_projects.rb
@@ -2,14 +2,17 @@ class CreateProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :projects do |t|
       t.string :title
-      t.string :short_description
       t.text :full_description
       t.string :category
       t.string :subcategory
-      t.string :pictures
-      t.integer :total_budget
       t.date :start_date
       t.date :finish_date
+      t.string :short_description
+      t.string :picture
+      t.float :latitude
+      t.float :longitude
+      t.string :address
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
