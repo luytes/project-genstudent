b6e7dcdc58bedd9401015f22d4097710245ca1f8
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-05-24 14:55:04 +0200

    generated the views and added pictures to it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524124242) do
+ActiveRecord::Schema.define(version: 20170524125416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,8 +46,9 @@ ActiveRecord::Schema.define(version: 20170524124242) do
     t.string   "degree"
     t.integer  "cost"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "student_picture"
     t.index ["user_id"], name: "index_students_on_user_id", using: :btree
   end
 
