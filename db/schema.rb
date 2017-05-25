2adebddb0fbad477f9dd895854920658aca8ecd3
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-05-25 16:20:27 +0200

    added a type attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525132102) do
+ActiveRecord::Schema.define(version: 20170525141701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170525132102) do
     t.string   "skills"
     t.text     "bio"
     t.string   "portfolio_url"
+    t.string   "type"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
