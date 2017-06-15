f8e9aaccd5cdd684923e61e8f57ea7f21a3e4441
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-06-15 16:03:25 +0200

    added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -148,7 +148,7 @@ Service.create!(
 Service.create(
     title: "Vacillation - Adolescence",
     description: "'Vacillation - Adolescence' is the second in a series of seven short dance films. The story is a snapshot of two teenagers in love and their surge of emotions and insecurities. Performed by two dancers, the story is portrayed through a classical ballet Pas de Deux with the London Thames path providing a wonderful backdrop.",
-    category: "film",
+    category: category.sample,
     address: address.sample,
     start_date: Faker::Date.backward(14).to_datetime,
     finish_date: Faker::Date.forward(23).to_datetime,
@@ -162,7 +162,7 @@ Service.create(
 Service.create(
     title: "DELTA ENDURING TAROT",
     description: "There is a savage beauty in the depths of the swamp. For each drop of blood spilled into those dark waters, the delta rises. She thrives. She continues to resist the encroaching gulf. So too is the way of her people, who despite endless struggle, continue to find love and wonder in the ruins of the natural world.",
-    category: "art",
+    category: category.sample,
     address: address.sample,
     start_date: Faker::Date.backward(14).to_datetime,
     finish_date: Faker::Date.forward(23).to_datetime,
@@ -176,7 +176,7 @@ Service.create(
 Service.create(
     title: "The Circuit: Urbiessa",
     description: "The purpose of this Kickstarter is to finance the very first episode of 'The Circuit' anthology. Help us make an incredible episode, and maybe more! It's up to you!",
-    category: "film",
+    category: category.sample,
     address: address.sample,
     start_date: Faker::Date.backward(14).to_datetime,
     finish_date: Faker::Date.forward(23).to_datetime,
@@ -185,6 +185,75 @@ Service.create(
     what_i_need_from_you: Faker::Lorem.paragraph(2),
     what_i_will_do_for_you: "Remember that you are unique. Do not sell yourself short. Do not sell the world short. This is your life – love it, live it. One life, one chance – grab it.",
     price: 12,
+  )
+Service.create(
+    title: "Sembene Across Africa",
+    description: "Europeans dominated African culture until African independence in the late 1950s and early 1960s. They ran the schools, newspapers, TV and movie theaters. African language was outlawed in many places; in French West Africa, the birthplace of Ousmane Sembene, Africans were evenb forbidden to use cameras.
+      Many Africans lost their connection with their cultures, but Sembene, an unlikely Senegalese hero, was determined to give African stories back to the African people. Sembene was kicked out of school at age 13 and worked for nearly 25 years as a manual laborer. He taught himself how to write at the age of 30, and soon was writing novels that inspired the battle for African independence.",
+    category: category.sample,
+    address: address.sample,
+    start_date: Faker::Date.backward(14).to_datetime,
+    finish_date: Faker::Date.forward(23).to_datetime,
+    remote_picture_url: "https://ksr-ugc.imgix.net/assets/016/462/864/bcdfa44c85b3ef1ffe22f4cab4278fda_original.jpg?w=680&fit=max&v=1493559775&auto=format&q=92&s=560af71426da774ac4e66542f4fec41d",
+    owner: Student.all.sample,
+    what_i_need_from_you: Faker::Lorem.paragraph(2),
+    what_i_will_do_for_you: "Remember that you are unique. Do not sell yourself short. Do not sell the world short. This is your life – love it, live it. One life, one chance – grab it.",
+    price: 25,
+  )
+
+Service.create(
+    title: "PANS",
+    description: "Set in a quiet beach town in grunge-saturated 1994, Pans puts a feminist twist on the classic, Peter Pan. Wynne Darling, 15, is physically broken, socially displaced, and emotionally torn between her newly separated parents.
+      With nowhere to turn, Wynne hooks up with a new crowd led by bad boy skateboarder, Pete, and his riot grrrl sister, Belle. Surrounded by 'lost boys' and with the help of found friendship, Wynne learns what it means to grow up of her own free will...",
+    category: category.sample,
+    address: address.sample,
+    start_date: Faker::Date.backward(14).to_datetime,
+    finish_date: Faker::Date.forward(23).to_datetime,
+    remote_picture_url: "https://ksr-ugc.imgix.net/assets/016/384/675/91b99f791f6925bc29d11d26499ac3e9_original.jpg?w=680&fit=max&v=1493083120&auto=format&q=92&s=756a07dcee33c807a4067ac1c05f18dc",
+    owner: Student.all.sample,
+    what_i_need_from_you: Faker::Lorem.paragraph(2),
+    what_i_will_do_for_you: "Remember that you are unique. Do not sell yourself short. Do not sell the world short. This is your life – love it, live it. One life, one chance – grab it.",
+    price: 423,
+  )
+
+Service.create(
+    title: "V",
+    description: "V is a confrontational and challenging coming-of-age drama that uses vampire mythology and horror genre tropes to explore femininity and sexual abuse. Think: Fish Tank meets Let the Right One In. The film slowly reveals how Minnie, a punchy word-a-second teenage girl, became a vampire as a result of being groomed online. Her vampirism is a physical embodiment of the damage caused.",
+    category: category.sample,
+    address: address.sample,
+    start_date: Faker::Date.backward(14).to_datetime,
+    finish_date: Faker::Date.forward(23).to_datetime,
+    remote_picture_url: "https://ksr-ugc.imgix.net/assets/016/463/044/b27f1f064831d084bbb2f467ad675b0f_original.png?w=680&fit=max&v=1493561075&auto=format&lossless=true&s=2133d8de7e7c407920e687ea9562bc7a",
+    owner: Student.all.sample,
+    what_i_need_from_you: Faker::Lorem.paragraph(2),
+    what_i_will_do_for_you: "Remember that you are unique. Do not sell yourself short. Do not sell the world short. This is your life – love it, live it. One life, one chance – grab it.",
+    price: 451,
+  )
+Service.create(
+    title: "BETTER EATING",
+    description: "Better Eating International is a 501(c)(3) nonprofit organization that is inspiring compassion and sparing animals from miserable lives and cruel deaths through smart, personalized educational videos. Our inclusive and empowering model will help create a future where animals are valued as unique individuals and no longer exploited for human gain. With a diverse and talented team armed with the latest technologies, we'll give people the knowledge and tools they’ll need to live more ethically.",
+    category: category.sample,
+    address: address.sample,
+    start_date: Faker::Date.backward(14).to_datetime,
+    finish_date: Faker::Date.forward(23).to_datetime,
+    remote_picture_url: "https://ksr-ugc.imgix.net/assets/016/227/143/0f05f81dad388a7fe1e1cd87ec83690f_original.png?w=680&fit=max&v=1491971620&auto=format&lossless=true&s=56ee3ea5adc789214b34fa75b566cb3d",
+    owner: Student.all.sample,
+    what_i_need_from_you: Faker::Lorem.paragraph(2),
+    what_i_will_do_for_you: "Remember that you are unique. Do not sell yourself short. Do not sell the world short. This is your life – love it, live it. One life, one chance – grab it.",
+    price: 425,
+  )
+Service.create(
+    title: "FOODIES",
+    description: "Foodies is a gonzo and artful horror-comedy film about young love, killer Kombucha, and mind-expanding science. When awkward loser Avery can't get his on-again off-again lover Sarah to commit, he decides to bio-hack his way back onto the dating scene. With the help of some genetically-altered health drinks, he sets out to pursue his romantic fantasies, utilizing yolked jocks to act as shared-consciousness avatars to score babes. It isn't long though before he finds the meat market is much more terrifying than he remembered...",
+    category: category.sample,
+    address: address.sample,
+    start_date: Faker::Date.backward(14).to_datetime,
+    finish_date: Faker::Date.forward(23).to_datetime,
+    remote_picture_url: "https://ksr-ugc.imgix.net/assets/016/435/830/8f6e2d4e566a01d85de1c0e5e1f92b60_original.JPG?w=680&fit=max&v=1493345613&auto=format&q=92&s=c1380c99f4b8ecb100125384d84e6ab3",
+    owner: Student.all.sample,
+    what_i_need_from_you: Faker::Lorem.paragraph(2),
+    what_i_will_do_for_you: "Remember that you are unique. Do not sell yourself short. Do not sell the world short. This is your life – love it, live it. One life, one chance – grab it.",
+    price: 342,
   )
 
 puts 'Finished!'
