0ee9e319bad61b6537a77b063de95fc4fb5e43ec
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-07-07 14:53:16 +0200

    fixxed. added permit keys in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,8 +26,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,
+                                      :company_name, :company_description])
     # only if you want the user to bae able to update his username
     # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
