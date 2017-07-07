7e4b5136f7c30611178c2dc60cc021a77d52d266
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-07-07 16:55:30 +0200

    fixxed now working. and added less space between stars

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -12,7 +12,9 @@ class ServicesController < ApplicationController
   def show
     @review = Review.new
     @user = current_user
-    # Shows me all orders with the service id of the service I am looking at right now
+    # Shows me all orders with the service id of the service
+    # I am looking at right now which are paid and have my customer id
+    # So this code here basically searches if the service i am looking right now have these conditions
     @order = Order.where(service_id: @service.id, state: 'paid', customer: @user.customer_id)
 
     count = 0
