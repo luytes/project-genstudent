74262636a11ed89618afb49cba9e1b813cc3df55
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-07-15 17:05:09 +0200

    If a customer subscribes to a service the admin will be notified via email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Welcome to Genstudent')
     # This will render a view in `app/views/user_mailer`!
   end
+
+  def subscribed(order, user)
+    @order = order
+    @user = user
+    mail(to: 'info@genstudent.com', subject: "#{@user.full_name} subscribed to #{@order.service.title}")
+  end
 end
