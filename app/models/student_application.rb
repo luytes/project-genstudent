395d88aaefbf7c4605b4018b8cbe1ca1cc9f43fc
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-07-25 16:42:19 +0800

    added student application validations

--- app/models/student_application.rb
+++ app/models/student_application.rb
@@ -1,2 +1,12 @@
 class StudentApplication < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :sex, presence: true
+  validates :major, presence: true
+  validates :country, presence: true
+  validates :email, presence: true
+  validates :skills, presence: true
+  validates :city, presence: true
+  validates :university, presence: true
+
 end
